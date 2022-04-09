1e504dd1252b127a1037a8d51ec8d2bdd37288a1
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-09 01:02:03 -0300

    Solve Hardest Problem Ever (easy)

---
+++ ch1/hardest_problem_ever.cpp
@@ -0,0 +1,11 @@
+/*
+ * UVa 12478 - Hardest Problem Ever (Easy)
+ * author: alexandrecorlet
+ */
+
+#include <iostream>
+using namespace std;
+int32_t main() {
+    cout << "KABIR" << '\n';
+    return 0;
+}
