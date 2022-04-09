81a8fd357ff24b5c4033bd63192bb3ff8228da32
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-09 01:00:34 -0300

    Solve carrots

---
+++ ch1/carrots.cpp
@@ -0,0 +1,21 @@
+/*
+ * Kattis - Solving for Carrots
+ * author: alexandrecorlet
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, p;
+    cin >> n >> p;
+    while (n--) { 
+        string description; 
+        cin >> description; 
+    }
+    cout << p << '\n';
+    return 0;
+}
