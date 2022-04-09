c9f34ac7c2886f376b0e8072518e9b9be88248bf
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-09 01:02:53 -0300

    Solve The Last Problem

---
+++ ch1/the_last_problem.cpp
@@ -0,0 +1,14 @@
+/*
+ * Kattis - The Last Problem
+ * author: alexandrecorlet
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main() {
+    string s;
+    getline(cin, s);
+    cout << "Thank you, " << s 
+         << ", and farewell!" << endl;
+    return 0;
+}
