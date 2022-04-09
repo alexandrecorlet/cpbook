bdc50bb0771557fcca6d25f5ff0ee5f25d3a9d96
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-09 00:59:52 -0300

    Solve Back to the past

---
+++ ch1/back_to_the_past.cpp
@@ -0,0 +1,12 @@
+/*
+ * UVa 13025 - Back to the Past
+ * author: alexandrecorlet
+ */
+
+#include <iostream>
+using namespace std;
+
+int32_t main() {
+    cout << "May 29, 2013 Wednesday" << '\n';
+    return 0;
+}
