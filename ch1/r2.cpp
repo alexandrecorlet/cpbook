a97dfda50df40314359e450b0da65dd9d561070d
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-09 01:02:25 -0300

    Solve r2

---
+++ ch1/r2.cpp
@@ -0,0 +1,16 @@
+/*
+ * Kattis - R2
+ * author: alexandrecorlet
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int r1, s;
+    cin >> r1 >> s;
+    cout << 2*s - r1 << '\n';
+    return 0;
+}
