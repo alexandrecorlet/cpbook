e4abf4bd0896ad4fc661b0ac5dca0568d2370178
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-10 17:05:47 -0300

    Solve Time Loop

---
+++ ch1/timeloop.cpp
@@ -0,0 +1,15 @@
+/*
+ * Kattis - Stuck In A Time Loop
+ *
+ * author: alexandrecorlet
+ */
+
+#include <iostream>
+using namespace std;
+int32_t main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        cout << i << " Abracadabra\n";
+    return 0;
+}
