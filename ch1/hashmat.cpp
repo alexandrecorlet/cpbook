5d01a2814236a8f713ff5a7bc79b7d7ab34329b9
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-10 18:23:22 -0300

    Solve Hashmat the Brave Warrior

---
+++ ch1/hashmat.cpp
@@ -0,0 +1,17 @@
+/*
+ * Kattis: Hashmat the Brave Warrior
+ * author: Alexandre Corlet
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int32_t main() {
+    ll x, y;
+    while (cin >> x >> y)
+        cout << abs(x - y) << '\n';
+    return 0;
+}
