c9331ec2a482d8f4c77fc2625fd68e1e6f885104
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-10 17:12:26 -0300

    Solve A Different Problem

---
+++ ch1/different_problem.cpp
@@ -0,0 +1,17 @@
+/*
+ * Kattis - A Different Problem
+ * author: Alexandre Corlet
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int32_t main() {
+    ll x, y;
+    while (cin >> x >> y)
+        cout << abs(x - y) << '\n';
+    return 0;
+}
