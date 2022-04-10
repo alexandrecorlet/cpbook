6b2fa52f4476933c307bd5183c55ca7be84dab4f
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-04-10 16:20:05 -0300

    Solve Celebrity Jeopardy

---
+++ ch1/celebrity_jeopardy.cpp
@@ -0,0 +1,16 @@
+/*
+ * UVa 1124 - Celebrity Jeopardy
+ *
+ * author: alexandre corlet
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+    string equation;
+    while (getline(cin, equation))
+        cout << equation << '\n';
+    return 0;
+}
