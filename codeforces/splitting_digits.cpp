4a92e4bc89473b5a498bf80fc131d80082c278ce
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-06-10 21:57:01 -0300

    Solve CF A. Splitting Digits

---
+++ codeforces/splitting_digits.cpp
@@ -0,0 +1,21 @@
+/*
+ * Codeforces: Splitting Into Digits
+ *
+ * author: uncoded
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", n);
+    for (int i = 1; i <= n; ++i)
+        printf("1 ");
+    printf("\n");
+    
+    return 0;
+}
