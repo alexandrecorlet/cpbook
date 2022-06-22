1197fab69c93284d0599ac0801a38c1369632c23
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-06-22 14:26:19 -0300

    Solve exercise 1.3.4.1 - 12 of CP4 chapter 1.

---
+++ ch1/evaluate_expr.py
@@ -0,0 +1,12 @@
+# Given a valid mathematical expressin involving '+', '-', '*', '/',
+# '(', and ')', in a single line, evaluate that expression.
+# author: Alexandre Corlet
+
+
+def main():
+    print(eval(input()))
+
+
+if __name__ == "__main__":
+    main()
+
